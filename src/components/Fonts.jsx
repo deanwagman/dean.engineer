6f4b8bfd07f75924e273828fc173ce3d400b3f11
Dean Wagman <deanwagman@deans-macbook.lan>
2020-05-27 20:28:51 -0700

    bass

---
+++ src/components/Fonts.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+
+const Fonts = () => (
+  <Helmet>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&family=Raleway:ital,wght@0,300;0,400;1,400&display=swap"
+      rel="stylesheet"
+    />
+  </Helmet>
+)
+
+export default Fonts
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -9,8 +9,9 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+import Header from "./Header"
 import "./layout.css"
+import Fonts from "./Fonts"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,6 +26,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <Fonts />
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
@@ -34,11 +36,6 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
       </div>
     </>
   )
